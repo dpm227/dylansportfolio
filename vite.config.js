c00be77338502a7d8e93f107f59bc42686dcaabe
Dylan McClellan <dmac@MacBook-Pro.local>
2025-10-23 21:49:31 -0400

    final base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from "node:url";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/<dylansportfolio>/",
+  base: "/dylansportfolio/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
